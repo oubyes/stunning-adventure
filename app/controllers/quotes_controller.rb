53df164f0c4456320ab4c38a4f96788ac7203fb4
Seybou <seybou@yahoo.com>
2019-07-19 03:26:46 +0000

    Chapter 14

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,13 +6,19 @@ class QuotesController < ApplicationController
 
     def new
         @quote = Quote.new
-  end
+    end
+   
    def create
-     Quote.create(quote_params)
-      redirect_to root_path
-  end
-  private
+     @quote = Quote.create(quote_params)
+     if @quote.invalid?
+     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+     end
+    redirect_to root_path
+    end
+    
+    private
     def quote_params
     params.require(:quote).permit(:saying, :author)
-end
+    end
+
 end
