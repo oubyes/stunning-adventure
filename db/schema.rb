6fc32abbd10f80af1a72603659064efa19d7008f
Seybou <seybou@yahoo.com>
2019-08-01 05:32:32 +0000

    lesson26

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_221437) do
+ActiveRecord::Schema.define(version: 2019_07_22_051620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_07_06_221437) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
   end
 
 end
